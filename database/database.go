d5b9d39480167c8beb312d8baabae4a16a5e9ec7
Tom-Shumi <tshumiya281@gmail.com>
2023-05-04 20:51:49 +0900

    mod: databaseの接続情報をconfig化

--- database/database.go
+++ database/database.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"zero-thinking-backend/config"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,9 +12,10 @@ var db *sql.DB
 
 // Init initializes database
 func Init() {
-	// c := config.GetConfig()
+	c := config.GetConfig()
 	var err error
-	db, err = sql.Open("mysql", "root:root@(localhost:3306)/zeroThinking?parseTime=true")
+	db, err = sql.Open(c.GetString("db.provider"),
+		fmt.Sprintf("%s:%s@(%s:%s)/zeroThinking?parseTime=true", c.GetString("db.user"), c.GetString("db.password"), c.GetString("db.domain"), c.GetString("db.port")))
 	if err != nil {
 		panic(err)
 	}
